8ec35ed4cc1c327b4a25cff4a3da87c7ddc57de6
Luiz Eduardo <luizeduardo2013051@gmail.com>
2026-06-02 23:00:43 +0000

    fix: correção de importações na landingPage

--- src/features/landing/pages/LandingPage.jsx
+++ src/features/landing/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import { BannerCTA } from "../components/BannerCTA";
 import { ComoFunciona } from "../components/ComoFunciona";
 import { Depoimentos } from "../components/Depoimentos";
 import { Footer } from "../components/Footer";
-import { Header } from "../components/header";
+import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
 import { ParaOngs } from "../components/ParaOngs";
 
