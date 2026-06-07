3ec9f0a3f469548586dab689dae6e3d2a228f178
luiz-eduardooo <luizeduardo2013051@gmail.com>
2026-06-07 19:57:49 -0300

    feat: Adição do login de usuário e a rota /login

--- src/app/routes/Router.jsx
+++ src/app/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RotaProtegidaUsuario } from "./RotaProtegidaUsuario";
 import { LandingPage } from "../../features/landing/pages/LandingPage";
+import LoginUsuario from "../../features/landing/pages/LoginUsuario";
 
 export function Router(){
    return( <BrowserRouter>
     <Routes>
         <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/login" element={<LoginUsuario/>}></Route>
         <Route element={RotaProtegidaUsuario}>
             {/* Adicionar depois !!!!! */}
         </Route>
---
+++ src/features/landing/pages/LoginUsuario.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useAuth } from '../../../shared/contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import { loginUsuario } from '../../usuarios/services/usuarioService';
+
+const LoginUsuario = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {entrar} = useAuth();
+    const navigate = useNavigate();
+
+    const handlerLogin = async (e)=>{
+        e.preventDefault();
+        try{
+            const data = await loginUsuario({email, password});
+            entrar({...data.user, token: data.token, tipo: 'usuario'})
+            navigate('/')
+        }
+        catch(error){
+            console.error(error)
+        }
+    }
+  return (
+    <div>
+      <form onSubmit={handlerLogin}>
+        <input type="email" name="email" value={email} required onChange={(el)=> setEmail(el.target.value)} />
+        <input type="password" name="password" value={password} required onChange={(el)=> setPassword(el.target.value)} />
+        <input type="submit" value="Entrar" />
+      </form>
+    </div>
+  )
+}
+
+export default LoginUsuario
