2cbe1c33d0974f289d8fd4baa470636cd693b22f
Luiz Eduardo <luizeduardo2013051@gmail.com>
2026-06-25 12:27:52 +0000

    feat: Adição do .env.example e ajuste pra entrar em produção

--- src/shared/api/client.js
+++ src/shared/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3003",
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
     headers:{
         "Content-Type": "application/json"
